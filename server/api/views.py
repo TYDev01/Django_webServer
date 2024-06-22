5490146dbc80a30e33047fdddc453979af9860fa
Anthony <anthonychinaemeru84@yahoo.com>
2024-06-22 21:15:35 +0100

    Add Exclamination mark

--- server/api/views.py
+++ server/api/views.py
@@ -5,15 +5,13 @@ from rest_framework.response import Response
 # Create your views here.
 @api_view(['GET'])
 def newVisitor(request):
-    visitName = request.query_params.get('visitor', 'Guest')
+    visitName = request.query_params.get('visitor', 'usersname')
     client_ip = request.META.get('REMOTE_ADDR')
     location = 'New York'
     data = {
         "client_ip": client_ip,
         "location": location,
-        "greeting": f"Hello, {visitName}"
+        "greeting": f"Hello, {visitName}!"
     }
     return Response(data)
 
-#Endpoint: [GET] <example.com>/api/hello?visitor="Mark"
-# (where < example.com> is your server origin)
